c11d911d54a31fc6d906aaf8cda977d39fa91af1
Julianralexander and John Remboldt <john@>
2017-10-09 20:07:22 -0700

    Added the colour change buttons

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,20 @@ $(document).ready(function(){
         $("img").fadeIn();
     });
 });
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
